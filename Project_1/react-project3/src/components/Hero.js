402841342d8ceb7d99810cc0d0c55e2fcef289bb
Austin-Holland1010 <austinpholland@yahoo.com>
2023-06-23 17:22:58 -0700

    19% module 2

---
+++ Project_1/react-project3/src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+    return (
+        <section className="hero">
+            <img className="hero--photo" src="https://www.nicepng.com/png/detail/809-8093522_travel-collage-collage-of-vacation-destinations.png"></img>
+            <h1 className="hero--herder">Online Experiences</h1>
+            <p className="hero--text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
+
+export default Hero
---
+++ Project_1/react-project3/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav className="airbnb-nav">
+            <img className="airbnb-img" src="https://logodownload.org/wp-content/uploads/2016/10/airbnb-logo-3-1.png" alt="Airbnb Logo"></img>
+        </nav>
+    )
+}
+
+export default Navbar
