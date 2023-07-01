9e2117982e9c3c0752ff3e5ca4f1fc5db1630f97
Austin-Holland1010 <austinpholland@yahoo.com>
2023-07-01 16:43:47 -0700

    Module 3 74% done

--- All_Projects/react-project4/src/components/Form.js
+++ All_Projects/react-project4/src/components/Form.js
@@ -2,36 +2,36 @@ import React from "react";
 
 function Form () {
     const [formData, setFormData] = React.useState(
-        {firstName: "", lastName: "", email: "", comments: ""}
+        {
+            firstName: "",
+            lastName: "",
+            email: "",
+            comments: "",
+            isFriendly: true,
+            employment: "",
+            favColor: ""
+        }
     )
     //const [firstName, setFirstName] = React.useState("")
     //const [lastName, setLastName] = React.useState("")
-    console.log(formData.firstName + " " + formData.lastName + " " + formData.email)
-    console.log(formData.comments)
-
+    //console.log(formData.firstName + " " + formData.lastName + " " + formData.email)
+    //console.log(formData.comments)
+    console.log(formData.employment + " " + formData.favColor)
 
     function handleChange(event) {
+        const {name, value, type, checked} = event.target
         setFormData(prevFormData => {
             return (
                 {
                     ...prevFormData,
-                    [event.target.name]: event.target.value
+                    [name]: type === "checkbox" ? checked : value
                 }
                 //  The [event.target.name] uses the name of the form
                 //  as the field of the object that is being set
             )
         })
     }
-    /*
-    function handleFirstNameChange (event) {
-        setFirstName(event.target.value)
-    }
 
-    function handleLastNameChange(event)
-    {
-        setLastName(event.target.value)    
-    }
-    */
     return (
         <form>
             <input
@@ -61,6 +61,71 @@ function Form () {
                 onChange={handleChange}
                 name="comments"
             />
+            <input 
+                type="checkbox"
+                id="isFriendly"
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
+            <br />
+            
+            <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />    
+            </fieldset>
+
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor"
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+            >
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
+
         </form>
     )
 }
